54765da37951f2079a5d0ade06f8be4fb8aad3d9
Marcin <marcinmoch757@gmail.com>
2023-02-15 23:34:10 +0100

    adding constraints to movies collection

--- src/main/java/dev/marcin/movieapi/movie/Movie.java
+++ src/main/java/dev/marcin/movieapi/movie/Movie.java
@@ -4,6 +4,7 @@ import dev.marcin.movieapi.review.Review;
 import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
@@ -15,9 +16,11 @@ import java.util.List;
 public class Movie {
     @Id
     private ObjectId id;
+    @Indexed(unique = true)
     private String imdbId;
     private String title;
     private String releaseDate;
+    @Indexed(unique = true)
     private String trailerLink;
     private String poster;
     private List<String> genres;
